08ac4b07a4831dd11c0329917d992219e0573670
AnazThaj <anaz.thaj@gmail.com>
2022-07-04 12:21:19 +0530

    Add a function that plays a single round of r-p-s

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,18 @@ function computerPlay() {
     return choice[Math.floor(Math.random() * choice.length)]
 }
 
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === 'rock' && computerSelection === "paper") {
+        return "You win!"
+    } else if(playerSelection === 'rock' && computerSelection === 'rock') {
+        return "It's a tie!"
+    } else {
+        return "YOU LOSE!!!!!"
+    }
+}
+// console.log(computerSelection)
+console.log(`You: ${playerSelection}, Computer: ${computerSelection}`)
+console.log(playRound(playerSelection, computerSelection))
